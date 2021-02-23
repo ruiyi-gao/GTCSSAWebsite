3a8dc7c1acf7a29f29f60b507a2ef59a96bf1ade
Marcus Zhou <other.marcus@icloud.com>
2021-02-23 12:49:29 -0500

    Email verification form for existing code

--- public/JavaScript/emailVerification.js
+++ public/JavaScript/emailVerification.js
@@ -1,43 +1,61 @@
 let sendAvailable = true;
 const emailPattern = /\S+@\S+\.\S+/;
 
-function sendEmail(){
+async function sendEmail(){
     const input = document.getElementById("email");
     const button = document.getElementById("emailCountDown");
     const alert = document.getElementById("emailAlert");
+    const recaptchaChallengeResponse = "ummmmm-wut-is-this"; // Get reCaptcha response and put it here.
+
+    const showError = (msg) => {
+        alert.innerText = msg;
+        alert.classList.remove("alert-success");
+        if (!alert.classList.contains("alert-warning")){
+            alert.classList.add("alert-warning");
+        }
+        alert.style.display = "block";
+    }
+
+    const showInfo = (msg) => {
+        alert.innerText = msg;
+        alert.classList.remove("alert-warning");
+        if (!alert.classList.contains("alert-success")){
+            alert.classList.add("alert-success");
+        }
+        alert.style.display = "block";
+    }
 
     if (sendAvailable){
         // Test whether the email address is valid
-        if (emailPattern.test(input.value) && input.value.includes("@gatech.edu")){
-            alert.innerText = "邮件已发送！如果没有收到，请检查垃圾邮件箱。"
-            alert.classList.remove("alert-warning");
-            if (!alert.classList.contains("alert-success")){
-                alert.classList.add("alert-success")
-            }
-            alert.style.display = "block";
+        if (emailPattern.test(input.value) && input.value.endsWith("@gatech.edu")){
+            try {
+                const req = await fetch(`/send_verification?mail=${input.value}&recaptchaResponse=${recaptchaChallengeResponse}`);
+                const res = await req.json();
 
-            sendAvailable = false;
-            let num = 10;
-            button.innerText = num;
-            const timer = setInterval(() => {
-                num = num - 1;
-                button.innerText = num;
-                if (num === 0){
-                    button.innerText = "获取验证码";
-                    clearInterval(timer);
-                    sendAvailable = true;
+                if (res.error && res.message) {
+                    showError(res.message);
+                } else {
+                    showInfo(res.message);
                 }
-            }, 1000);
-        } else {
-            alert.innerText = "请输入正确的GT邮箱"
-            alert.classList.remove("alert-success")
-            if (!alert.classList.contains("alert-warning")){
-                alert.classList.add("alert-warning")
+            } catch (e) {
+                showError(e.message);
+            } finally {
+                sendAvailable = false;
+                let num = 10;
+                button.innerText = num;
+                const timer = setInterval(() => {
+                    num = num - 1;
+                    button.innerText = num;
+                    if (num === 0){
+                        button.innerText = "获取验证码";
+                        clearInterval(timer);
+                        sendAvailable = true;
+                    }
+                }, 1000);
             }
-            alert.style.display = "block";
+        } else {
+            showError("请输入正确的GT邮箱");
         }
-
-        
     }
 }
 
