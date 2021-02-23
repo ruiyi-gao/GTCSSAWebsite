83a195973468d49e14a6d65356c453aab91c80dc
Marcus Zhou <other.marcus@icloud.com>
2021-02-23 13:45:44 -0500

    Complete email verification

--- src/const.ts
+++ src/const.ts
@@ -5,9 +5,11 @@ import assert from "assert";
 export const projectDir = path.dirname(__dirname);
 export const configDir = path.join(projectDir, "config");
 export const dataDir = path.join(projectDir, "data");
+export const templateDir = path.join(projectDir, "template");
 export const isProduction = process.env.NODE_ENV === "production"
 
 assert.ok(fs.existsSync(configDir), "Configuration directory must be present. Make sure submodules are initialized.");
+assert.ok(fs.existsSync(templateDir), "Template directory must be present.");
 
 // Make data dir if not exists
 fs.mkdirSync(dataDir, { recursive: true });
